2443892d81c6c4d7b7360f913dac7d9cbdfee3ab
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-24 12:29:25 +0300

    noon

--- project/Program.cs
+++ project/Program.cs
@@ -10,10 +10,10 @@
         const string FILENAME = "reports.txt";
 
         static string[] UnitName = new string[100];
-        static string[] ReportType = new string[100];
+        static TypeType[] ReportType = new TypeType[100];
         static int[] Priority = new int[100];
         static double[] Score = new double[100];
-        static string[] Status = new string[100];
+        static StatusType[] Status = new StatusType[100];
 
         static string[] LoadFile()
         {
@@ -41,38 +41,48 @@
             string[] shapedReport = report.Split(',');
             if (shapedReport.Length != 5) { return false; }
             string unit = shapedReport[0].Trim();
-            if (!Enum.TryParse<TypeType>(shapedReport[1].Trim(), out string type)  { return false; }
-            else if (!int.TryParse(shapedReport[2].Trim(), out int priority)  { return false; }
+            if (!Enum.TryParse<TypeType>(shapedReport[1].Trim(), out TypeType type))  { return false; }
+            else if (!int.TryParse(shapedReport[2].Trim(), out int priority))  { return false; }
             else if (priority < 0 || priority > 5) { return false; }
-            else if (!double.TryParse(shapedReport[3].Trim(), out double score)  { return false; }
+            else if (!double.TryParse(shapedReport[3].Trim(), out double score))  { return false; }
             else if (score < 0 || score > 100) { return false; }
-            else if (!Enum.TryParse<StatusType>(shapedReport[4].Trim(), out string status)  { return false; }
+            else if (!Enum.TryParse<StatusType>(shapedReport[4].Trim(), out StatusType status))  { return false; }
 
             else
             {
-                UnitName.Add(unit);
-                ReportType.Add(type);
-                Priority.Add(priority); 
-                Score.Add(score);
-                Status.Add(status);
+                UnitName[count] = unit;
+                ReportType[count] = type;
+                Priority[count] = priority; 
+                Score[count] = score;
+                Status[count] = status;
                 count++;
                 return true;
-
             }
         }
 
-        static void FilterGoodOnes()
+        static int FilterGoodOnes()
         {
             string[] reports = LoadFile();
             int count = 0;
             for (int i = 0; i < reports.Length; i++) 
-                {IsGoodReport(reports[i]), ref count)}
+                { IsGoodReport(reports[i], ref count); }
             Console.WriteLine($"Valid records: {count}");
             Console.WriteLine($"Invalid records: {reports.Length - count}");
+            return count;
         }
         static void Main()
         {
-            FilterGoodOnes();
+            int count =FilterGoodOnes();
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Unit Name: {UnitName[i]}");
+                Console.WriteLine($"Report Type: {ReportType[i]}");
+                Console.WriteLine($"Priority: {Priority[i]}");
+                Console.WriteLine($"Score: {Score[i]}");
+                Console.WriteLine($"Status: {Status[i]}");
+                Console.WriteLine("");
+            }
         }
     }
 }
+
